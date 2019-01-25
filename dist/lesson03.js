ecdf21be2c43c589f6327dee08f7d100185bb724
chuongcn <chuongchung.it@gmail.com>
2019-01-25 16:32:29 +0700

    Array number - Array string trong Typescript

---
+++ dist/lesson03.js
@@ -0,0 +1,18 @@
+"use strict";
+// Kiểu dữ liệu number
+var x;
+x = 100;
+console.log(x);
+// Array kiểu String
+var hocVien;
+hocVien = ["ChuongCN", "Thanh", "Thy", "Hân"];
+for (var i = 0; i < hocVien.length; i++) {
+    console.log(hocVien[i]);
+}
+// Array kiểu number
+var array2;
+array2 = [9, 10, 2, 100, 9876, 23];
+for (var i = 0; i < array2.length; i++) {
+    console.log(array2[i]);
+}
+//# sourceMappingURL=lesson03.js.map
---
+++ lesson03.ts
@@ -0,0 +1,19 @@
+// Kiểu dữ liệu number
+
+let x: number;
+x = 100;
+console.log(x);
+
+// Array kiểu String
+var hocVien: string[];
+hocVien = ["ChuongCN", "Thanh", "Thy", "Hân"];
+for (var i = 0; i < hocVien.length; i++) {
+  console.log(hocVien[i]);
+}
+
+// Array kiểu number
+var array2: number[];
+array2 = [9, 10, 2, 100, 9876, 23];
+for (var i = 0; i < array2.length; i++) {
+  console.log(array2[i]);
+}
